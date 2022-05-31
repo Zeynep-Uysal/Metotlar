db7de11063d9c0856196ff45737c04560749dc5b
Zeynep <Zeynep@DESKTOP-FPSILAM>
2022-06-01 01:50:19 +0300

    Add project files.

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+     internal class SepetManager
+    {
+        //naming convention - Metotların da ilk harfi büyük yazılır
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete eklendi : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat) 
+        {
+            Console.WriteLine("Sepete eklendi : " + urunAdi);
+        }
+    }
+}
